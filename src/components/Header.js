201303ff845a8ebe5eab623936a1ff0cf4a674d9
Fiqy <120717422+fiqyy@users.noreply.github.com>
2026-01-20 08:50:42 +0200

    Fixed the resume and the home path

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header () {
         <div>
             <nav>
                 <ul className="header-nav">
-                    <li className={`nav-item ${location.pathname === "/home" ? "active" : ""}`}> <Link to='/home'>Home</Link></li>
+                    <li className={`nav-item ${location.pathname === "/" ? "active" : ""}`}> <Link to='/'>Home</Link></li>
                     <li className={`nav-item ${location.pathname === "/about" ? "active" : ""}`}> <Link to='/about'>About</Link></li>
                     <li className={`nav-item ${location.pathname === "/projects" ? "active" : ""}`}> <Link to='/projects'>Projects</Link></li>
                     <li className={`nav-item ${location.pathname === "/contact" ? "active" : ""}`}> <Link to='/contact'>Contact</Link></li>
