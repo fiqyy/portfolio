4d16a51f592d70bc3bb132d63838cfe6b37951a8
Fiqy <fiqyyyy@gmail.com>
2026-09-01 19:10:50 +0300

    Updated my title in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="home">
       <h1 className="hi-text">Hi, I'm Youssef Fiqy</h1>
-      <h2 className='hi-text2'>I'm a Business Solutions Developer</h2>
+      <h2 className='hi-text2'>I'm an IT Business Solutions Developer</h2>
       <div className="button-container">
         <button className="home-button" onClick={() => navigate('/contact')}>Contact Me</button>
         <button className="home-button" onClick={() => navigate('/projects')}>Projects</button>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,22 @@
 import FlowingMenu from '../assets/modules/FlowingMenu'
-export default function Projects () {
-const Items = [
-  { link: 'https://to-do-list-app-uy9e.vercel.app/', text: 'To Do List App', image: '' },
-  { link: 'https://calculator-app-dun-rho.vercel.app/', text: 'Calculator App', image: '' },
-  { link: 'https://www.innoverstar.com/', text: 'Website For Innover Star ', image: '' },
-  { link: 'https://new-eslf.vercel.app/#achievements', text: 'Website For ESLF Law Firm', image: '' },
-];
-return (
-<div style={{ height: '300px', position: 'relative' }}>
-  <FlowingMenu items={Items}
-  speed={15}
-  textColor="#ffffff"
-  bgColor="transparent"
-  marqueeBgColor="rgb(2,99,99)"
-  marqueeTextColor="white"
-  borderColor="#ffffff"
-/>
-</div>
-)
+export default function Projects() {
+  const Items = [
+    { link: 'https://to-do-list-app-uy9e.vercel.app/', text: 'To Do List App', image: '' },
+    { link: 'https://calculator-app-dun-rho.vercel.app/', text: 'Calculator App', image: '' },
+    { link: 'https://www.innoverstar.com/', text: 'Website For Innover Star ', image: '' },
+    { link: 'https://new-eslf.vercel.app/#achievements', text: 'Website For ESLF Law Firm', image: '' },
+
+  ];
+  return (
+    <div style={{ height: '300px', position: 'relative' }}>
+      <FlowingMenu items={Items}
+        speed={15}
+        textColor="#ffffff"
+        bgColor="transparent"
+        marqueeBgColor="rgb(2,99,99)"
+        marqueeTextColor="white"
+        borderColor="#ffffff"
+      />
+    </div>
+  )
 }
